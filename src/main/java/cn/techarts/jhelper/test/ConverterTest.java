97af6b3e3ae056926b2b6fc362991e16e120fbeb
rocwon <pengfei@techarts.com.cn>
2021-10-31 18:06:09 +0800

    Appended a test case for bit Manipulation APIs

--- src/main/java/cn/techarts/jhelper/test/ConverterTest.java
+++ src/main/java/cn/techarts/jhelper/test/ConverterTest.java
@@ -3,6 +3,7 @@ package cn.techarts.jhelper.test;
 import org.junit.Test;
 
 import cn.techarts.jhelper.Converter;
+import cn.techarts.jhelper.Finder;
 import junit.framework.TestCase;
 
 public class ConverterTest {
@@ -35,5 +36,21 @@ public class ConverterTest {
 		Object obj = new Book(2);
 		var book = Converter.to(obj, Book.class);
 		TestCase.assertEquals(obj, book);
+	}
+	
+	@Test
+	public void testBitManipulations() {
+		int arg = 1234567890;
+		var val = Converter.toBytes(arg);
+		TestCase.assertEquals(arg, Converter.toInt(val));
+		
+		short arg1 = 12;
+		var val1 = Converter.toBytes(arg1);
+		TestCase.assertEquals(arg1, Converter.toShort(val1));
+		
+		byte arg3 = 0x00;
+		var val3 = Converter.toBooleans(arg3);
+		TestCase.assertEquals(false, val3[5]);
+		
 	}
 }
