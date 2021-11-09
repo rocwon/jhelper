b1ff066ea0d2f13e0fc3f10acea91e36d6be6507
rocwon <pengfei@techarts.com.cn>
2021-11-09 10:30:28 +0800

    Added a test case for methods Concat(separator, *)

--- src/main/java/cn/techarts/jhelper/test/ConcatorTest.java
+++ src/main/java/cn/techarts/jhelper/test/ConcatorTest.java
@@ -1,5 +1,7 @@
 package cn.techarts.jhelper.test;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import cn.techarts.jhelper.Concator;
@@ -11,5 +13,17 @@ public class ConcatorTest {
 	public void testContactArrays() {
 			var result = Concator.concat(new int[] {1,2,3,4,5,6}, new int[] {7,8,9,10,11,12,13,14,15,16});
 			TestCase.assertEquals(16, result.length);
+	}
+	
+	@Test
+	public void testConcatWithSeparator() {
+		var result = Concator.concat(',', "abc", "cd", "ef");
+		TestCase.assertEquals("abc,cd,ef", result);
+		result = Concator.concat(List.of("abc", "cd", "ef"), ',');
+		TestCase.assertEquals("abc,cd,ef", result);
+		var result1 = Concator.concat(',', 1, 0, 2 ,3 ,4, 5);
+		TestCase.assertEquals("1,0,2,3,4,5", result1);
+		result1 = Concator.concat(List.of(1, 0, 2 ,3 ,4, 5), ',');
+		TestCase.assertEquals("1,0,2,3,4,5", result1);
 	}
 }
